ba17c9bf1366ee1769e082b42022d9710dad1ae3
ramirs <ramirs2015@gmail.com>
2017-11-03 19:43:35 -0700

    updating app styles for single event view

--- scripts/uiManager.js
+++ scripts/uiManager.js
@@ -1,34 +1,34 @@
 (function () {
 
   //refs
-  var scrollHeader = document.getElementById('scroll-header');
-  var scrollHeaderTitle = document.getElementById('scroll-header-title');
-  var eventFab = document.getElementById('event-fab');
+  // var scrollHeader = document.getElementById('scroll-header');
+  // var scrollHeaderTitle = document.getElementById('scroll-header-title');
+  // var eventFab = document.getElementById('event-fab');
 
   //event listener and actions
-  document.getElementById('scroll-container').onscroll = function () {
-
-    if(200 - this.scrollTop >= 80) {
-      scrollHeader.style.height = (200 - this.scrollTop) + 'px';
-    } else if (this.scrollTop >= 120) {
-      scrollHeader.style.height = 80 + 'px';
-    }
-
-    if(this.scrollTop <= 120) {
-      scrollHeaderTitle.style.paddingTop = 80 - (this.scrollTop / 3) + 'px';
-      scrollHeaderTitle.style.fontSize = 1.0 - (this.scrollTop / 300) + 'em';
-    } else if (this.scrollTop > 120){
-      scrollHeaderTitle.style.paddingTop = 30 + 'px';
-      scrollHeaderTitle.style.fontSize = '0.6em';
-    }
-
-    if(this.scrollTop >= 285) {
-      eventFab.style.display = 'inline';
-    } else {
-      eventFab.style.display = 'none';
-    }
-
-  }
+  // document.getElementById('scroll-container').onscroll = function () {
+  //
+  //   if(200 - this.scrollTop >= 80) {
+  //     scrollHeader.style.height = (200 - this.scrollTop) + 'px';
+  //   } else if (this.scrollTop >= 120) {
+  //     scrollHeader.style.height = 80 + 'px';
+  //   }
+  //
+  //   if(this.scrollTop <= 120) {
+  //     scrollHeaderTitle.style.paddingTop = 80 - (this.scrollTop / 3) + 'px';
+  //     scrollHeaderTitle.style.fontSize = 1.0 - (this.scrollTop / 300) + 'em';
+  //   } else if (this.scrollTop > 120){
+  //     scrollHeaderTitle.style.paddingTop = 30 + 'px';
+  //     scrollHeaderTitle.style.fontSize = '0.6em';
+  //   }
+  //
+  //   if(this.scrollTop >= 285) {
+  //     eventFab.style.display = 'inline';
+  //   } else {
+  //     eventFab.style.display = 'none';
+  //   }
+  //
+  // }
 
   var appTitle = document.getElementById('app-title');
   var titleDisplay = document.getElementById('title-display');
