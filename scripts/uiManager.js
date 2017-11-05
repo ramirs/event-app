4612c61f69199910f6d2bc91c6ec2d6357df81c3
ramirs <ramirs2015@gmail.com>
2017-11-05 15:34:14 -0800

    updating header styles to avoid lag... even though that was pretty cool

--- scripts/uiManager.js
+++ scripts/uiManager.js
@@ -1,22 +1,14 @@
 (function () {
 
-  var appHead = document.getElementById('app-ui-header');
-  var sixLogo = document.getElementById('br_logo');
-  var appTitle = document.getElementById('app-title');
+  var appHeadMini = document.getElementById('app-ui-header-mini');
   document.getElementById('app-view-schedule').onscroll = function () {
-
     if(this.scrollTop <= 360) {
-        appHead.style.height = (420 - this.scrollTop) + 'px';
-        sixLogo.style.display = 'inline';
-        appTitle.style.display = 'none';
+        appHeadMini.style.display = 'none';
     } else {
-      appHead.style.height = 60 + 'px';
-      sixLogo.style.display = 'none';
-      appTitle.style.display = 'inline';
+      appHeadMini.style.display = 'inline';
     }
   };
 
-  sixLogo.style.display = 'inline';
-  appTitle.style.display = 'none';
+  appHeadMini.style.display = 'none';
 
 })();
