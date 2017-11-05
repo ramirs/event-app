801b47d5302894cbb333e6c9d151209c531fc452
ramirs <ramirs2015@gmail.com>
2017-11-05 15:14:48 -0800

    updating event listeners for header features

--- scripts/uiManager.js
+++ scripts/uiManager.js
@@ -1,51 +1,22 @@
 (function () {
 
-  //refs
-  // var scrollHeader = document.getElementById('scroll-header');
-  // var scrollHeaderTitle = document.getElementById('scroll-header-title');
-  // var eventFab = document.getElementById('event-fab');
-
-  //event listener and actions
-  // document.getElementById('scroll-container').onscroll = function () {
-  //
-  //   if(200 - this.scrollTop >= 80) {
-  //     scrollHeader.style.height = (200 - this.scrollTop) + 'px';
-  //   } else if (this.scrollTop >= 120) {
-  //     scrollHeader.style.height = 80 + 'px';
-  //   }
-  //
-  //   if(this.scrollTop <= 120) {
-  //     scrollHeaderTitle.style.paddingTop = 80 - (this.scrollTop / 3) + 'px';
-  //     scrollHeaderTitle.style.fontSize = 1.0 - (this.scrollTop / 300) + 'em';
-  //   } else if (this.scrollTop > 120){
-  //     scrollHeaderTitle.style.paddingTop = 30 + 'px';
-  //     scrollHeaderTitle.style.fontSize = '0.6em';
-  //   }
-  //
-  //   if(this.scrollTop >= 285) {
-  //     eventFab.style.display = 'inline';
-  //   } else {
-  //     eventFab.style.display = 'none';
-  //   }
-  //
-  // }
-
+  var appHead = document.getElementById('app-ui-header');
+  var sixLogo = document.getElementById('br_logo');
   var appTitle = document.getElementById('app-title');
-  var titleDisplay = document.getElementById('title-display');
-  var titleTimer;
   document.getElementById('app-view-schedule').onscroll = function () {
-    appTitle.style.height = '65px';
-    titleDisplay.style.display = 'none';
-    clearTimeout(titleTimer);
-    titleTimer = setTimeout(function() {
-      titleDisplay.style.display = 'inline';
-      appTitle.style.height = '90px';
-    }, 750);
 
-  }
+    if(this.scrollTop <= 360) {
+        appHead.style.height = (420 - this.scrollTop) + 'px';
+        sixLogo.style.display = 'inline';
+        appTitle.style.display = 'none';
+    } else {
+      appHead.style.height = 60 + 'px';
+      sixLogo.style.display = 'none';
+      appTitle.style.display = 'inline';
+    }
+  };
 
-  //init state
-  scrollHeader.style.height = '200px';
-  eventFab.style.display = 'none';
+  sixLogo.style.display = 'inline';
+  appTitle.style.display = 'none';
 
 })();
